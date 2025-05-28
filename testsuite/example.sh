7494b0ae52406efb24794005e2ff5af5ddcf2457
sophia-gh <stesta4@kent.edu>
2025-05-28 15:43:18 -0400

    update to example.sh schema
    updated example.sh to work from a source file input, plans to do so to cli.sh once source code files for set and set_pos are available, update to cmakelist in testsuite to copy input cpp files into the build directory

--- testsuite/example.sh
+++ testsuite/example.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
-input=$(cat <<EOF
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.srcML.org/srcML/src" revision="1.0.0" language="C++" filename="test.cpp">
-<decl_stmt><decl><type><name>int</name></type> <name>y</name></decl>;</decl_stmt>
-</unit>
-EOF
-)
+input=$(srcml int.cpp)
+# input=$(cat <<EOF
+# <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+# <unit xmlns="http://www.srcML.org/srcML/src" revision="1.0.0" language="C++" filename="test.cpp">
+# <decl_stmt><decl><type><name>int</name></type> <name>y</name></decl>;</decl_stmt>
+# </unit>
+# EOF
+# )
 output=$(echo "$input" | ../bin/nameCollector )
-expected="x is a int global in C++ file: test.cpp"
+expected="x is a int global in C++ file: int.cpp"
 if [[ "$output" != "$expected" ]]; then
     echo "Test failed!"
     echo "Expected: '$expected'"
