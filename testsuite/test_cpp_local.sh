21d915d015cfedd35149bdfcd3d0ee1c018fee3a
sophia-gh <stesta4@kent.edu>
2025-05-30 15:20:40 -0400

    test_local.sh failing test case
    for lines such as: `int i, j` the type of the names after the comma is not given by nameCollector

--- testsuite/test_cpp_local.sh
+++ testsuite/test_cpp_local.sh
@@ -8,6 +8,17 @@ int multiply(int a, int b){
     int return_value = a*b;
     return return_value;
 }
+
+int main(){
+    int k = 8, j, factorial;
+    int product = multiply(10, 12);
+
+    for( int i=0; i < 4; i++){
+        factorial = multiply(i, i++);
+    }
+    return 0;
+}
+
 EOF
 
 input=$(srcml test_local.cpp --position)
@@ -15,12 +26,19 @@ output=$(echo "$input" | ./nameCollector )
 expected="multiply is a int function in C++ file: test_local.cpp at 1:5
 a is a int parameter in C++ file: test_local.cpp at 1:18
 b is a int parameter in C++ file: test_local.cpp at 1:25
+return_value is a int local in C++ file: test_local.cpp at 2:9
+main is a int function in C++ file: test_local.cpp at 6:5
+k is a int local in C++ file: test_local.cpp at 7:9
+j is a int local in C++ file: test_local.cpp at 7:16
+factorial is a int local in C++ file: test_local.cpp at 7:19
+product is a int local in C++ file: test_local.cpp at 8:9
+i is a int local in C++ file: test_local.cpp at 10:14
 return_value is a int local in C++ file: test_local.cpp at 2:9"
 
 if [[ "$output" != "$expected" ]]; then
     echo "Test test_local failed!"
     echo "Expected: '$expected'"
-    echo "Got:      '$output'"
+    echo "Got: '$output'"
     exit 1
 fi
 
