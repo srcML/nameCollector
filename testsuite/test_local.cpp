0fe92dc4b1af62ffe204f3bf6366a3c7cb05ae2b
sophia-gh <stesta4@kent.edu>
2025-05-30 11:55:08 -0400

    added test for local variables
    - tests output correctly identifies local variable declared inside a function

---
+++ testsuite/test_local.cpp
@@ -0,0 +1,4 @@
+int multiply(int a, int b){
+    int return_value = a*b;
+    return return_value;
+}
---
+++ testsuite/test_local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+input=$(srcml test_local.cpp)
+output=$(echo "$input" | ../bin/nameCollector )
+expected="multiply is a int function in C++ file: test_local.cpp
+a is a int parameter in C++ file: test_local.cpp
+b is a int parameter in C++ file: test_local.cpp
+return_value is a int local in C++ file: test_local.cpp"
+
+if [[ "$output" != "$expected" ]]; then
+    echo "Test failed!"
+    echo "Expected: '$expected'"
+    echo "Got:      '$output'"
+    exit 1
+fi
+
+# Repeat tests
+
+exit 0
