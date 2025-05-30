bc750072fecac343746938a735ab4c9dd6f4a5d6
sophia-gh <stesta4@kent.edu>
2025-05-30 15:29:08 -0400

    added test case to  test global
    added multi-line declaration test case to the global file, encounters the same issue with types as in the local test case, this problem is outlined in issue #18

--- testsuite/test_cpp_global.sh
+++ testsuite/test_cpp_global.sh
@@ -6,7 +6,7 @@ cat <<EOF > test_global.cpp
 int global_int=55;
 bool global_bool;
 float global_float;
-char global_char;
+char global_char, a, b;
 EOF
 
 input=$(srcml test_global.cpp --position)
@@ -21,7 +21,9 @@ output=$(echo "$input" | ./nameCollector )
 expected="global_int is a int global in C++ file: test_global.cpp at 1:5
 global_bool is a bool global in C++ file: test_global.cpp at 2:6
 global_float is a float global in C++ file: test_global.cpp at 3:7
-global_char is a char global in C++ file: test_global.cpp at 4:6"
+global_char is a char global in C++ file: test_global.cpp at 4:6
+a is a char global in C++ file: test_global.cpp at 4:19
+b is a char global in C++ file: test_global.cpp at 4:22"
 
 if [[ "$output" != "$expected" ]]; then
     echo "Test test_global failed!"
