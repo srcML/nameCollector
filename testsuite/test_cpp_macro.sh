b57d4bd5b4cdc2089614ad73f218efa70a86e62f
sophia-gh <stesta4@kent.edu>
2025-06-27 13:35:28 -0400

    test_cpp_macro update

--- testsuite/test_cpp_macro.sh
+++ testsuite/test_cpp_macro.sh
@@ -14,26 +14,8 @@ EOF
 
 input=$(srcml test_macro.hpp --position)
 output=$(echo "$input" | ./nameCollector )
-expected="TEST_CLASS_HPP is a macro in C++ file: test_class.hpp at 2:9
-Counter is a class in C++ file: test_class.hpp at 4:7
-Counter is a constructor in C++ file: test_class.hpp at 6:5
-Counter is a constructor in C++ file: test_class.hpp at 7:5
-value is a int parameter in C++ file: test_class.hpp at 7:17
-~Counter is a destructor in C++ file: test_class.hpp at 8:5
-operator++ is a Counter& function in C++ file: test_class.hpp at 9:14
-operator -- is a Counter& function in C++ file: test_class.hpp at 10:14
-display is a void function in C++ file: test_class.hpp at 11:10
-counter_value is a int * field in C++ file: test_class.hpp at 14:9
-Counter is a constructor in C++ file: test_class.cpp at 4:10
-Counter is a constructor in C++ file: test_class.cpp at 6:10
-value is a int parameter in C++ file: test_class.cpp at 6:22
-~Counter is a destructor in C++ file: test_class.cpp at 8:10
-operator++ is a Counter& function in C++ file: test_class.cpp at 15:19
-operator -- is a Counter& function in C++ file: test_class.cpp at 20:19
-display is a void function in C++ file: test_class.cpp at 25:15
-main is a int function in C++ file: test_class.cpp at 29:5
-defaultCounter is a Counter local in C++ file: test_class.cpp at 30:13
-seven is a Counter local in C++ file: test_class.cpp at 31:13"
+expected="TEST_CLASS_HPP is a macro in C++ file: test_macro.hpp at 2:9
+MACRO is a macro in C++ file: test_macro.hpp at 6:9"
 
 if [[ "$output" != "$expected" ]]; then
     echo "Test test_cpp_class failed!"
