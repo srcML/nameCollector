c562df1dea7026bf2b92ef4009796fdd4bc2914a
sophia-gh <stesta4@kent.edu>
2025-10-31 13:07:47 -0400

    added test case for nested goto statements

--- testsuite/test_cpp_labels.sh
+++ testsuite/test_cpp_labels.sh
@@ -23,20 +23,32 @@ int main() {
     //switch statement case labels, should not collect 
     switch(i){
         case 10: 
-            std::cout<<"loop worked";
+            std::cout<<"loop worked" <<std::endl;
             break;
         case 9: 
-            std::cout<"uh oh!";
+            std::cout<<"uh oh!";
             break;
         default: 
-            std::cout <<"how?;
+            std::cout <<"how?";
             break; 
-    }
+    };
+
+    //nested goto statements
+    int x; 
+    x = 1;
+    outerLabel:
+        if(x<5){ goto innerLabel;}
+        else { goto endLabel;}
+        innerLabel:
+            x +=1;
+            std::cout<<"less Than" << std::endl;
+            goto outerLabel; 
+    endLabel:
+        std::cout<<"greater than!!!" << std::endl;
     
     return 0; 
 }
     
-
 EOF
 
 input=$(srcml test_label.cpp --position)
@@ -44,7 +56,11 @@ output=$(echo "$input" | ./nameCollector )
 expected="main is a int function in C++ file: test_label.cpp:3:5
 i is a int local in C++ file: test_label.cpp:5:9
 startOfLoop is a label in C++ file: test_label.cpp:6:5
-endOfLoop is a label in C++ file: test_label.cpp:15:5"
+endOfLoop is a label in C++ file: test_label.cpp:15:5
+x is a int local in C++ file: test_label.cpp:32:9
+outerLabel is a label in C++ file: test_label.cpp:34:5
+innerLabel is a label in C++ file: test_label.cpp:37:9
+endLabel is a label in C++ file: test_label.cpp:41:5"
 
 if [[ "$output" != "$expected" ]]; then
     echo "Test test_cpp_label failed!"
