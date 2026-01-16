4fa5dbf6eb4eba5e01409b028cdae31f8f04a976
sophia-gh <stesta4@kent.edu>
2026-01-16 13:12:08 -0500

    added test cases
    test passes

--- testsuite/test_c_enum.sh
+++ testsuite/test_c_enum.sh
@@ -2,10 +2,17 @@
 
 cat <<EOF > test_c_enum.c
 #include <stdio.h>
-enum Empty { };
 enum Colors {RED, YELLOW, BLUE};
+// with assigned value to one
+enum Difficulty {EASY = 1, MEDIUM, HARD}; 
+// with all assigned value
+enum Values {FIRST = 1, SECOND = 2, FOURTH = 4 };
+// anon and immediate declaration
+enum {ON = 1, OFF = 0} light = OFF; 
 
 int main(){
+    enum Colors myColor;
+    enum Values myValue = SECOND; 
     return 0; 
 }
 
@@ -13,16 +20,32 @@ EOF
 
 input=$(srcml test_c_enum.c --position)
 output=$(echo "$input" | ./nameCollector )
-expected="Empty is a enum in C file: test_c_enum.c:2:6
-Colors is a enum in C file: test_c_enum.c:3:6
-RED is a field in C file: test_c_enum.c:3:14
-YELLOW is a field in C file: test_c_enum.c:3:19
-BLUE is a field in C file: test_c_enum.c:3:27
-main is a int function in C file: test_c_enum.c:5:5"
+expected="Colors is a enum in C file: test_c_enum.c:2:6
+RED is a field in C file: test_c_enum.c:2:14
+YELLOW is a field in C file: test_c_enum.c:2:19
+BLUE is a field in C file: test_c_enum.c:2:27
+Difficulty is a enum in C file: test_c_enum.c:4:6
+EASY is a field in C file: test_c_enum.c:4:18
+MEDIUM is a field in C file: test_c_enum.c:4:28
+HARD is a field in C file: test_c_enum.c:4:36
+Values is a enum in C file: test_c_enum.c:6:6
+FIRST is a field in C file: test_c_enum.c:6:14
+SECOND is a field in C file: test_c_enum.c:6:25
+FOURTH is a field in C file: test_c_enum.c:6:37
+ON is a field in C file: test_c_enum.c:8:7
+OFF is a field in C file: test_c_enum.c:8:15
+light is a enum global in C file: test_c_enum.c:8:24
+main is a int function in C file: test_c_enum.c:10:5
+myColor is a enum Colors local in C file: test_c_enum.c:11:17
+myValue is a enum Values local in C file: test_c_enum.c:12:17"
 
 expected_enum=(
-  "Empty is a enum in C file: test_c_enum.c:2:6"
-  "Colors is a enum in C file: test_c_enum.c:3:6"
+  "Colors is a enum in C file: test_c_enum.c:2:6"
+  "Difficulty is a enum in C file: test_c_enum.c:4:6"
+  "Values is a enum in C file: test_c_enum.c:6:6"
+  "light is a enum global in C file: test_c_enum.c:8:24"
+  "myColor is a enum Colors local in C file: test_c_enum.c:11:17"
+  "myValue is a enum Values local in C file: test_c_enum.c:12:17"
 )
 
 for enum in "${expected_enum[@]}"; do
