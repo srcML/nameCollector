f8e290bce757037cfde000977c546482cf3872cc
sophia-gh <stesta4@kent.edu>
2026-02-20 15:32:13 -0500

    basic case for labels in c
    no need to test corner cases, passes as written

--- testsuite/test_c_label.sh
+++ testsuite/test_c_label.sh
@@ -2,31 +2,8 @@
 
 cat <<EOF > test_c_label.c
 #include <stdio.h>
-// label defined in macro
-#define LABEL(a){                    \
-    __label__ one, two, exit;        \
-    if(a==2){                        \
-        goto two;                    \
-    } else {                         \
-        goto one;                    \
-    }                                \
-    two:                             \
-        printf("input is two");      \
-        goto exit;                   \
-    one:                             \
-        printf("input is not two");  \
-        goto exit;                   \
-    exit: printf("");                \
-}                                    \
- 
-
-// basic form __label__ label_name.
-// goto label_name
-// label_name: {code}
 
 int main(){
-    // local labels
-    __label__ conditionA, conditionB, conditionC;
     int x=5;
     if(x == 6){
         goto conditionA;
@@ -35,7 +12,7 @@ int main(){
     } else{
         goto conditionC; 
     }
-    // nested labels, one without a forward declaration
+
     conditionA:
         printf("conditonA");
         goto end;
@@ -47,29 +24,24 @@ int main(){
         goto end; 
     end:
         printf("\n");
-        LABEL(2);
         return 0;
 }
 EOF
 
 input=$(srcml test_c_label.c --position)
 output=$(echo "$input" | ./nameCollector )
-expected="LABEL is a macro in C file: test_c_label.c:3:9
-main is a int function in C file: test_c_label.c:9:5
-conditionA is a __label__ local in C file: test_c_label.c:11:15
-conditionB is a __label__ local in C file: test_c_label.c:11:27
-conditionC is a __label__ local in C file: test_c_label.c:11:39
-x is a int local in C file: test_c_label.c:12:9
-conditionA is a label in C file: test_c_label.c:21:5
-conditonB is a label in C file: test_c_label.c:24:5
-conditionC is a label in C file: test_c_label.c:27:5
-end is a label in C file: test_c_label.c:30:5"
+expected="main is a int function in C file: test_c_label.c:3:5
+x is a int local in C file: test_c_label.c:4:9
+conditionA is a label in C file: test_c_label.c:13:5
+conditonB is a label in C file: test_c_label.c:16:5
+conditionC is a label in C file: test_c_label.c:19:5
+end is a label in C file: test_c_label.c:22:5"
 
 expected_label=(
-  "conditionA is a label in C file: test_c_label.c:21:5"
-  "conditonB is a label in C file: test_c_label.c:24:5"
-  "conditionC is a label in C file: test_c_label.c:27:5"
-  "end is a label in C file: test_c_label.c:30:5"
+  "conditionA is a label in C file: test_c_label.c:13:5"
+  "conditonB is a label in C file: test_c_label.c:16:5"
+  "conditionC is a label in C file: test_c_label.c:19:5"
+  "end is a label in C file: test_c_label.c:22:5"
 )
 
 for label in "${expected_label[@]}"; do
