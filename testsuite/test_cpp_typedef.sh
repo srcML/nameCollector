f667fdeee577cb80b284025de5d8a699499b23f2
sophia-gh <stesta4@kent.edu>
2026-04-03 14:38:40 -0400

    cleaned up test_cpp_typedef
    compiles properly
    still fails as expected until issue #41 is addressed

--- testsuite/test_cpp_typedef.sh
+++ testsuite/test_cpp_typedef.sh
@@ -4,8 +4,9 @@
 
 cat <<EOF > test_typedef.cpp
 #include <vector>
+#include <iostream>
 typedef int Integer; 
-typedef vector<int> int_vector;
+typedef std::vector<int> int_vector;
 typedef char* char_array[5];    //array of char ptr
 typedef int (*functionPtr)(int, int);  // with fxn pointer
 
@@ -26,18 +27,18 @@ EOF
 
 input=$(srcml test_typedef.cpp --position)
 output=$(echo "$input" | ./nameCollector )
-expected="Integer is a typedef in C++ file: test_typedef.cpp:2:13
-int_vector is a typedef in C++ file: test_typedef.cpp:3:21
-char_array is a typedef in C++ file: test_typedef.cpp:4:15
-functionPtr is a int function in C++ file: test_typedef.cpp:5:15
-Point is a struct in C++ file: test_typedef.cpp:8:16
-x is a int field in C++ file: test_typedef.cpp:9:9
-y is a int field in C++ file: test_typedef.cpp:9:12
-pt is a typedef of struct in C++ file: test_typedef.cpp:10:3
-v is a int field in C++ file: test_typedef.cpp:13:9
-w is a int field in C++ file: test_typedef.cpp:13:12
-anon_struct_typedef is a typedef of struct in C++ file: test_typedef.cpp:14:3
-main is a int function in C++ file: test_typedef.cpp:16:5"
+expected="Integer is a typedef in C++ file: test_typedef.cpp:4:13
+int_vector is a typedef in C++ file: test_typedef.cpp:5:21
+char_array is a typedef in C++ file: test_typedef.cpp:6:15
+functionPtr is a int function in C++ file: test_typedef.cpp:7:15
+Point is a struct in C++ file: test_typedef.cpp:10:16
+x is a int field in C++ file: test_typedef.cpp:11:9
+y is a int field in C++ file: test_typedef.cpp:11:12
+pt is a typedef of struct in C++ file: test_typedef.cpp:12:3
+v is a int field in C++ file: test_typedef.cpp:15:9
+w is a int field in C++ file: test_typedef.cpp:15:12
+anon_struct_typedef is a typedef of struct in C++ file: test_typedef.cpp:16:3
+main is a int function in C++ file: test_typedef.cpp:18:5"
 
 if [[ "$output" != "$expected" ]]; then
     echo "Test test_cpp_typedef failed!" 
