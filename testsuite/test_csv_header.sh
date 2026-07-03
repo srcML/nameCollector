a8ef4dacf6a32d5bce4c7783190e52d0192ae4df
Joshua Behler <jbehler1@kent.edu>
2026-07-03 16:33:47 -0400

    Fix csv header tests failing on Mac

--- testsuite/test_csv_header.sh
+++ testsuite/test_csv_header.sh
@@ -44,55 +44,40 @@ int main(){
 EOF
 
 input=$(srcml test_csv_header.cpp --position)
-echo "$input" | ../bin/nameCollector --csv -o test_csv_header.csv
-mapfile -t output < test_csv_header.csv
-output_text=$(cat test_csv_header.csv)
+output=$(echo "$input" | ./nameCollector --csv)
+expected="Name,Type,Category,File,Position,Language,Stereotype
+multiply,int,function,test_csv_header.cpp,2:5,C++,
+a,int,parameter,test_csv_header.cpp,2:18,C++,
+b,int,parameter,test_csv_header.cpp,2:25,C++,
+return_value,int,local,test_csv_header.cpp,3:9,C++,
+Shapes,,class,test_csv_header.cpp,8:7,C++,
+Shapes,,constructor,test_csv_header.cpp,11:9,C++,
+Shapes,,constructor,test_csv_header.cpp,12:9,C++,
+calculateArea,int,function,test_csv_header.cpp,14:13,C++,
+length,int,parameter,test_csv_header.cpp,14:31,C++,
+width,int,parameter,test_csv_header.cpp,14:43,C++,
+localInsideClassMethod,int,local,test_csv_header.cpp,15:17,C++,
+StaticIntLocal,static int,local,test_csv_header.cpp,16:24,C++,
+nonStaticLocal,int,local,test_csv_header.cpp,17:17,C++,
+borderCharacter,char,field,test_csv_header.cpp,22:14,C++,
+main,int,function,test_csv_header.cpp,25:5,C++,
+k,int,local,test_csv_header.cpp,26:9,C++,
+j,int,local,test_csv_header.cpp,26:16,C++,
+big_number,int,local,test_csv_header.cpp,26:19,C++,
+product,int,local,test_csv_header.cpp,27:9,C++,
+i,int,local,test_csv_header.cpp,29:14,C++,
+next_one,int,local,test_csv_header.cpp,30:13,C++,
+collection,std::vector<int>,local,test_csv_header.cpp,34:22,C++,
+number,auto,local,test_csv_header.cpp,35:14,C++,
+even,bool,local,test_csv_header.cpp,36:33,C++,
+odd,bool,local,test_csv_header.cpp,37:20,C++,"
 
-expected=("Name,Type,Category,File,Position,Language,Stereotype"
-"multiply,int,function,test_csv_header.cpp,2:5,C++,"
-"a,int,parameter,test_csv_header.cpp,2:18,C++,"
-"b,int,parameter,test_csv_header.cpp,2:25,C++,"
-"return_value,int,local,test_csv_header.cpp,3:9,C++,"
-"Shapes,,class,test_csv_header.cpp,8:7,C++,"
-"Shapes,,constructor,test_csv_header.cpp,11:9,C++,"
-"Shapes,,constructor,test_csv_header.cpp,12:9,C++,"
-"calculateArea,int,function,test_csv_header.cpp,14:13,C++,"
-"length,int,parameter,test_csv_header.cpp,14:31,C++,"
-"width,int,parameter,test_csv_header.cpp,14:43,C++,"
-"localInsideClassMethod,int,local,test_csv_header.cpp,15:17,C++,"
-"StaticIntLocal,static int,local,test_csv_header.cpp,16:24,C++,"
-"nonStaticLocal,int,local,test_csv_header.cpp,17:17,C++,"
-"borderCharacter,char,field,test_csv_header.cpp,22:14,C++,"
-"main,int,function,test_csv_header.cpp,25:5,C++,"
-"k,int,local,test_csv_header.cpp,26:9,C++,"
-"j,int,local,test_csv_header.cpp,26:16,C++,"
-"big_number,int,local,test_csv_header.cpp,26:19,C++,"
-"product,int,local,test_csv_header.cpp,27:9,C++,"
-"i,int,local,test_csv_header.cpp,29:14,C++,"
-"next_one,int,local,test_csv_header.cpp,30:13,C++,"
-"collection,std::vector<int>,local,test_csv_header.cpp,34:22,C++,"
-"number,auto,local,test_csv_header.cpp,35:14,C++,"
-"even,bool,local,test_csv_header.cpp,36:33,C++,"
-"odd,bool,local,test_csv_header.cpp,37:20,C++,")
-
-expected_text=$(printf "%s\n" "${expected[@]}")
-
-if [ ${#output[@]} -ne ${#expected[@]} ]; then
+if [[ "$output" != "$expected" ]]; then
     echo "Test test_csv_header failed!"
-    echo "Expected: '$expected_text'"
-    echo "Got: '$output_text'"
+    echo "Expected: '$expected'"
+    echo "Got:      '$output'"
     exit 1
-else
-    for i in "${!expected[@]}"; do
-      if [[ "${output[$i]}" != "${expected[$i]}" ]]; then
-        echo "Test test_csv_header failed on line $((i + 1))!"
-        echo "   Expected: '${expected[$i]}'"
-        echo "   Got: '${output[$i]}'"
-        exit 1
-      fi
-    done
 fi
-
 echo "Test test_csv_header passed!"
 
 exit 0
